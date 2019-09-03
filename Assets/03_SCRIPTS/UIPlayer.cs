f7cd62abea2db3a88cc33412a650050042531c34
Simon <dedieu.simon01@gmail.com>
2019-09-03 15:56:45 +0200

    change framework to 4.7.2

--- Assets/03_SCRIPTS/UIPlayer.cs
+++ Assets/03_SCRIPTS/UIPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class UIPlayer : MonoBehaviour
 {
+    
     [SerializeField] Text textScore;
     //// Start is called before the first frame update
     void Start()
