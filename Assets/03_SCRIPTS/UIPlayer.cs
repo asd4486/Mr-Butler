adb79c4591df629b25c65e0189db1f897cfb7172
Simon <dedieu.simon01@gmail.com>
2019-09-03 17:19:09 +0200

    change timer

--- Assets/03_SCRIPTS/UIPlayer.cs
+++ Assets/03_SCRIPTS/UIPlayer.cs
@@ -7,14 +7,32 @@ public class UIPlayer : MonoBehaviour
 {
     
     [SerializeField] Text textScore;
+    [SerializeField] Text textTimer;
+    public float timeLeft = 60.0f;
+
     //// Start is called before the first frame update
     void Start()
     {
         SetScore(0);
     }
 
+    void Update(){
+        SetTimer();
+    }
+
     public void SetScore(int score)
     {
         textScore.text = "Score : " + score;
     }
+
+    public void SetTimer()
+    {
+        timeLeft -= Time.deltaTime;
+        textTimer.text ="Timer : "+ (timeLeft).ToString("0");
+        if (timeLeft < 0)
+        {
+            Debug.Log("Dead");
+        }
+    }
+    
 }
